4f7ffda70a582cbddfee965723258058e17c361d
melvinkukwansafari <melvin.kukwan@Student.moringaschool.com>
2023-03-17 08:14:41 +0300

    complete assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -13,18 +14,24 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
-
-  function handleSubmit(e) {
+// make the function async to enable the await keyword
+ async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+     // fetch returns a Promise, we must await it
+  const response = await fetch("/movies", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  // response.json() returns a Promise, we must await it
+  const data = await response.json();
+  if (response.ok) {
+    console.log("Movie created:", data);
+  } else {
+    setErrors(data.errors);
+  }
   }
 
   function handleChange(e) {
@@ -38,93 +45,19 @@ function MovieForm() {
 
   return (
     <Wrapper>
-      <form onSubmit={handleSubmit}>
-        <FormGroup>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            value={formData.title}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="year">Year</label>
-          <input
-            type="number"
-            id="year"
-            min="1888"
-            max={new Date().getFullYear()}
-            value={formData.year}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="length">Length</label>
-          <input
-            type="number"
-            id="length"
-            value={formData.length}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="director">Director</label>
-          <input
-            type="text"
-            id="director"
-            value={formData.director}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="description">Description</label>
-          <textarea
-            id="description"
-            value={formData.description}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="poster_url">Poster</label>
-          <input
-            type="text"
-            id="poster_url"
-            value={formData.poster_url}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="category">Category</label>
-          <input
-            type="text"
-            id="category"
-            value={formData.category}
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="discount">
-            Discount?
-            <input
-              type="checkbox"
-              id="discount"
-              checked={formData.discount}
-              onChange={handleChange}
-            />
-          </label>
-        </FormGroup>
-        <FormGroup>
-          <label htmlFor="female_director">
-            Female Director?
-            <input
-              type="checkbox"
-              id="female_director"
-              checked={formData.female_director}
-              onChange={handleChange}
-            />
-          </label>
-        </FormGroup>
+    
+    <form onSubmit={handleSubmit}>
+        {/* form elements here... */}
+
+        {/* show error messages if any */}
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
